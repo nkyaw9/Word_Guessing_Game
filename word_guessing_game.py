bed9104daaf2a9469e5c76f8dc24144113c45d6e
nkyaw9 <nyaingpk@gmail.com>
2026-04-02 03:42:58 -0400

    added new game logic and cleaned up code

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -5,64 +5,81 @@ base_url = "https://random-words-api.kushcreates.com/"
 
 def main():
 
+    used_letters = []
+    counter = 1
+
     player_name = input("Enter your name: ")
+    print(f"Welcome to Word Guessing Game {player_name}")
 
     while True:
+
         try:
+
             difficulty = int(input("Difficulty -> Easy (1) | Medium (2) | Hard (3): "))
+            if difficulty > 3 or difficulty < 1:
+                raise Exception("Enter 1, 2, or 3")
+            
+            if difficulty == 1:
+                attempts = 3
+                word_length = 5
+            elif difficulty == 2:
+                attempts = 3
+                word_length = 7
+            elif difficulty == 3:
+                attempts = 3
+                word_length = 10
+
             break
+
         except ValueError:
             print("Input a number corresponding to the difficulty")
+        except Exception as e:
+            print(e)
     
-    if difficulty == 1:
-        attempts = 3
-        word_length = 5
-    elif difficulty == 2:
-        attempts = 4
-        word_length = 7
-    elif difficulty == 3:
-        attempts = 5
-        word_length = 10
-
     word_dic = get_random_word(word_length)
+    if word_dic == -1:
+        return
+    
     word = str(word_dic['word'])
     word_category = str(word_dic['category'])
     word_copy = word
-    if word == -1:
-        return
-    
-    num_of_char = len(word)
-
     my_list = ["_"] * word_length 
-    counter = 1
-    
+    spaces = [i for i, ch in enumerate(word) if ch.isspace()]
+    for index in spaces:
+        my_list[index] = " "
+
     print("Word Category:", word_category, "| Attempts:", attempts)
     
     while True:
-        if attempts == 0:
-            print("You lose")
-            print(f"Answer: {word} | Total guesses: {counter - 1}")
-            break
-        if "".join(my_list) == word:
-            print("You win!")
-            print(f"Total guesses: {counter - 1}")
-            break
+        
         print(" ".join(my_list))
-        char_guess = input(f"Guess #{counter} ({attempts}) |->: ")
-        if len(char_guess) != 1:
-            print("Input only 1 character")
+
+        char_guess = input(f"Guess #{counter} ({attempts}) -> ")
+
+        if char_guess in used_letters or char_guess in my_list:
+            print("Already Guessed/Found")
             continue
+
         counter += 1
-        if char_guess in my_list:
-            print("Already found")
-        elif char_guess in word:
+
+        if char_guess in word:
             print("Found")
             word_copy = place_letter(my_list, word, char_guess, word_copy)
         else:
+            used_letters.append(char_guess)
             print("No match")
             attempts -= 1
             reveal_letter = random.randrange(0, len(word_copy), 1)
             word_copy = place_letter(my_list, word, word_copy[reveal_letter], word_copy)
+        
+        if attempts == 0:
+            print("You lose")
+            print(f"Answer: {word} | Total guesses: {counter - 1}")
+            break
+        if "".join(my_list) == word or char_guess == word:
+            print("You win!")
+            print(f"Answer: {word} | Total guesses: {counter - 1}")
+            break
         
 def place_letter(my_list, word, char_guess, word_copy):
     index = 0
@@ -83,6 +100,5 @@ def get_random_word(word_length):
         print("Error:", response.status_code, response.text)
         return -1
 
-
-
-main()
+if __name__ == '__main__':
+    main()
